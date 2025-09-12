825b03c68800edfa5b1d27a8cc01976b50a5a7d0
CoolRobots <darppdudee@gmail.com>
2025-09-11 21:37:23 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelightAutoScan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelightAutoScan.java
@@ -1,34 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.util.InterpLUT;
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
-import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-public class limelightAutoScan {
+@TeleOp
+public class limelightAutoScan extends LinearOpMode {
+    private Limelight3A limelight;
+    private String pattern;
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare motor ok
+        // Declare stuff ok
         // Absolutely yes make ID's match configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("fL");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("bL");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("fR");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("bR");
-        DcMotor flyWheel = hardwareMap.dcMotor.get("flyWheel");
+       limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
 
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead ok.
-        // See the note about this earlier on this page ok.
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map ok
         IMU imu = hardwareMap.get(IMU.class, "imu");
@@ -39,19 +34,12 @@ public class limelightAutoScan {
         //hi twin ok
         imu.initialize(parameters);
 
-        InterpLUT lut = new InterpLUT();
 
-//Adding each val with a key ok
-        lut.add(1.1, 0.2);
-        lut.add(2.7, .5);
-        lut.add(3.6, 0.75);
-        lut.add(4.1, 0.9);
-        lut.add(5, 1);
-//generating final equation ok
-        lut.createLUT();
-
-
-
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.start();
+        limelight.pipelineSwitch(0);
+        pattern="N/A";
 
 
         waitForStart();
@@ -59,42 +47,33 @@ public class limelightAutoScan {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
-            // Absolutely yes this button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
+            LLResult result = limelight.getLatestResult();
+            if (result != null && result.getPipelineIndex()==0) {
+                pattern="Green Purple Purple";
 
-            double distance = 0;  //0: iam limelight
+            } else if(result != null && result.getPipelineIndex()==1) {
+                pattern="Purple Green Purple";
+            }
+            else if(result != null && result.getPipelineIndex()==2) {
+                pattern="Purple Purple Green";
+            }
+            else{
+                pattern="N/A";
+        }
             if(gamepad1.a){
-                flyWheel.setPower(lut.get(distance));
+                limelight.pipelineSwitch(0);
             }
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-            // Rotate the movement direction counter to the bot's rotation ok
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            rotX = rotX * 1.1;  // Counteract imperfect strafing ok
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1] ok
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(-backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(-backRightPower);
-
+            if(gamepad1.b){
+                limelight.pipelineSwitch(1);
+            }
+            if(gamepad1.y){
+                limelight.pipelineSwitch(2);
+            }
+            if(gamepad1.right_stick_button){
+                telemetry.addData("dat tieu eats cats", pattern);
+            }
+            telemetry.addData("Pattern is: ", pattern);
+            telemetry.update();
         }
     }
 }
